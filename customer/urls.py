cbc1325a565f4dd7dee892b5c6cba48c590b49f9
shaghayegh shahbazy <shsh6184@gmail.com>
2024-01-10 16:35:15 +0330

    add signup with verify email

--- customer/urls.py
+++ customer/urls.py
@@ -5,4 +5,6 @@ from .views import *
 urlpatterns = [
     path('login/', AuthLoginUser.as_view()),
     path('logout/', LogoutAPIView.as_view()),
+    path('register/', UserRegistration.as_view()),
+    path('email-verify/', VerifyEmail.as_view(), name='email-verify'),
 ]
