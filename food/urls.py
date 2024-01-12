add8dbb8ca7f17bcf116fa8e329719b675fbc44a
shaghayegh shahbazy <shsh6184@gmail.com>
2024-01-12 16:23:42 +0330

    add get all foods and search foods

--- food/urls.py
+++ food/urls.py
@@ -4,4 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('get-food/<int:pk>/', GetFood.as_view()),
+    path('get-foods/', GetAllFoods.as_view()),
 ]
--- food/views.py
+++ food/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from rest_framework import generics, filters
+from rest_framework.permissions import IsAuthenticated
 
 from food.models import Food
 from food.serializers import FoodSerializer
@@ -7,3 +8,12 @@ from food.serializers import FoodSerializer
 class GetFood(generics.RetrieveAPIView):
     serializer_class = FoodSerializer
     queryset = Food.objects.all()
+    permission_classes = (IsAuthenticated,)
+
+
+class GetAllFoods(generics.ListAPIView):
+    serializer_class = FoodSerializer
+    queryset = Food.objects.all()
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', 'category', ]
+    permission_classes = (IsAuthenticated,)
