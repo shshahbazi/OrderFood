67288cf51af8a843e902221a91986fb495b62dbf
shaghayegh shahbazy <shsh6184@gmail.com>
2024-01-12 17:05:55 +0330

    add get user's favorite restaurant

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics, filters
 from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
 
+from customer.models import Profile
 from .models import Restaurant
 from .serializers import RestaurantSerializer
 
@@ -17,3 +19,12 @@ class GetAllRestaurants(generics.ListAPIView):
     filter_backends = [filters.SearchFilter]
     search_fields = ['name', 'description', 'address', 'category', 'features__name']
     permission_classes = (IsAuthenticated,)
+
+
+class GetFavRestaurant(generics.ListAPIView):
+    serializer_class = RestaurantSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        profile = get_object_or_404(Profile, id=self.request.user.id)
+        return profile.fav_restaurant.all()
