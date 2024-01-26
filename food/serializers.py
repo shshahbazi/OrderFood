9534d331d68f2ba05465cad9a8efd8398a0a87c6
shaghayegh shahbazy <shsh6184@gmail.com>
2024-01-27 02:49:45 +0330

    edit food and restaurant serializers

--- food/serializers.py
+++ food/serializers.py
@@ -7,3 +7,11 @@ class FoodSerializer(serializers.ModelSerializer):
     class Meta:
         model = Food
         fields = '__all__'
+
+    def to_representation(self, instance):
+        return_dict = dict(super().to_representation(instance))
+
+        choice_dict = {k[0]: k[1] for k in instance.CATEGORY_CHOICES}
+        return_dict['category'] = choice_dict[return_dict['category']]
+
+        return return_dict
--- restaurant/serializers.py
+++ restaurant/serializers.py
@@ -15,3 +15,11 @@ class RestaurantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Restaurant
         fields = '__all__'
+
+    def to_representation(self, instance):
+        return_dict = dict(super().to_representation(instance))
+
+        choice_dict = {k[0]: k[1] for k in instance.CATEGORY_CHOICES}
+        return_dict['category'] = choice_dict[return_dict['category']]
+
+        return return_dict
