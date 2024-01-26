80705dafc19fee2356be1d175918ed893b0eeff5
shaghayegh shahbazy <shsh6184@gmail.com>
2024-01-26 10:53:09 +0330

    add restaurant features serializer

--- restaurant/serializers.py
+++ restaurant/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 
-from .models import Restaurant
+from .models import Restaurant, RestaurantFeature
+
+
+class RestaurantFeatureSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RestaurantFeature
+        fields = '__all__'
 
 
 class RestaurantSerializer(serializers.ModelSerializer):
+    features = RestaurantFeatureSerializer(read_only=True, many=True)
+
     class Meta:
         model = Restaurant
         fields = '__all__'
